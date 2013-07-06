1ee4a50b5ad1f93c3065d434b8f122b96ef6b149
Alex Ling <alex.ling.9495@gmail.com>
2013-07-06 01:31:42 -0700

    Added Kyle's Rotation code, added files for player ship model

--- Assets/Assets/shipRotation.cs
+++ Assets/Assets/shipRotation.cs
@@ -1,19 +1,30 @@
+
 using UnityEngine;
 using System.Collections;
 
-public class shipRotation : MonoBehaviour {
+public class shipRotation : MonoBehaviour
+{
 
     private Vector3 rotDir = new Vector3(0, 0, 0);
     private KeyCode rgt = KeyCode.D;
     private KeyCode lft = KeyCode.A;
-    private float angle = 40;
+    private KeyCode shift = KeyCode.LeftShift;
+    private float angleAdjustSpeed = 1;
+    private float rollSpeed = 25;
+    private float currAngle = 0;
+    private float rollAngle = 0;
+    private int rollDir = 1;
 
-	void Start () {
-	
-	}
-	
-	
-	void Update () {
+    private readonly float maxAngle = 35; //Readonly is the same as final
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
 
         float rotZ = transform.eulerAngles.z;
 
@@ -21,20 +32,90 @@ public class shipRotation : MonoBehaviour {
         {
             rotZ -= 360;
         }
-        
-        rotDir.Set(0, 0, -rotZ);
 
+        //rotDir.Set(0, 0, -rotZ);
+        if (rollAngle - rollSpeed >= 0)
+        {
+            transform.Rotate(new Vector3(0, 0, rollSpeed * rollDir));
+            rollAngle -= rollSpeed;
+            Debug.Log(rollAngle);
+        }
+        else if (rollAngle > 0)
+        {
+
+            transform.Rotate(new Vector3(0, 0, (rollAngle) * rollDir));
+            rollAngle = 0;
+
+        }
         if (Input.GetKey(rgt))
         {
-           rotDir.z -= 1 * angle;
+            if (currAngle - angleAdjustSpeed > -maxAngle)
+            {
+                currAngle -= angleAdjustSpeed * 1;
+                transform.Rotate(new Vector3(0, 0, -angleAdjustSpeed));
+            }
+            if (rollAngle == 0)
+            {
+                rollDir = -1;
+            }
         }
-        if (Input.GetKey(lft))
+        else if (Input.GetKey(lft))
         {
-            rotDir.z += 1 * angle;
+            if (currAngle + angleAdjustSpeed < maxAngle)
+            {
+                currAngle += angleAdjustSpeed * 1;
+                transform.Rotate(new Vector3(0, 0, +angleAdjustSpeed));
+            }
+            if (rollAngle == 0)
+            {
+                rollDir = 1;
+            }
+        }
+        else if (rollAngle == 0)
+        {
+            if (currAngle + angleAdjustSpeed < 0)
+            {
+                transform.Rotate(new Vector3(0, 0, angleAdjustSpeed));
+                currAngle += angleAdjustSpeed;
+                if (rollAngle == 0)
+                {
+                    rollDir = 1;
+                }
+                //Debug.Log("moving left");
+            }
+            else if (currAngle - angleAdjustSpeed > 0)
+            {
+                transform.Rotate(new Vector3(0, 0, -angleAdjustSpeed));
+                currAngle -= angleAdjustSpeed;
+                if (rollAngle == 0)
+                {
+                    rollDir = -1;
+                }
+                //Debug.Log("returning right");
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 0, 0 - currAngle));
+                currAngle = 0;
+                if (rollAngle == 0)
+                {
+                    rollDir = 1;
+                }
+            }
+        }
+        if (Input.GetKey(shift) && rollAngle == 0)
+        {
+            rollAngle = +(360);
+
         }
 
-        print(rotDir.z);
-        transform.Rotate(rotDir.normalized);
-	
-	}
+        //Debug.Log(currAngle);
+
+       
+
+        //print(rotDir.z);
+        //transform.Rotate(rotDir.normalized);
+
+    }
 }
+
