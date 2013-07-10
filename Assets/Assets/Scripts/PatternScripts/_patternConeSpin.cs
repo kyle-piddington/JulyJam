94de1422773171443dea29ac0429ae9531fea452
kyle-piddington <kpiddy@gmail.com>
2013-07-09 23:25:08 -0700

    Fixed some meta files.

--- Assets/Assets/Scripts/PatternScripts/_patternConeSpin.cs
+++ Assets/Assets/Scripts/PatternScripts/_patternConeSpin.cs
@@ -6,7 +6,7 @@ public class _patternConeSpin : _patternConeFire {
     public float angleMomentum;
 	// Use this for initialization
 
-    void Update()
+    new void Update()
     {
         base.Update();
         base.offsetAngle += angleMomentum * Time.deltaTime;
